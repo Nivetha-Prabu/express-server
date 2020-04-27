b19a256afa6c5a23344eae3f48421748ea450850
Nivetha-Prabu <jinivethaprabu@gmail.com>
2020-04-27 13:26:38 +0530

    Add sequelize todo

--- playground/basic-sqlite.js
+++ playground/basic-sqlite.js
@@ -1,8 +1,68 @@
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(undefined, undefined, undefined, {
 	'dialect' : 'sqlite',
-	'storage' : 'basic-sqlite-database.sqlite'
+	'storage' : __dirname+ 'basic-sqlite-database.sqlite'
 });
-sequelize.sync().then(function(){
+
+var Todo =sequelize.define('todo', {
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: [1,250]
+		}
+	},
+	completed: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
+	}
+});
+
+sequelize.sync({
+ // force: true
+}).then(function(){
 	console.log('Everything is synced');
+Todo.findById(2).then(function (todo) {
+	if(todo) {
+		console.log(todo.toJSON());
+	} else {
+		console.log('Todo not found');
+	}
 });
+});
+// 	Todo.create({
+// 		description: 'I am new to sqlite'
+// 		// completed:false
+// 	}). then(function (todo) {
+// 		return Todo.create({
+// 			description: 'Welcome to sqlite nivetha'
+// 		});
+// 	}) .then(function() {
+// 		// return Todo.findById(1)
+// 		return Todo.findAll({
+// 			where: {
+// 				// completed: false
+// 				description: {
+// 					$like: '%nivetha'
+// 				}
+// 			}
+// 		});
+// 	}). then(function(todos) {
+// 		if(todos) {
+// 			todos.forEach(function(todo) {
+// 			console.log(todo.toJSON());	
+// 		});
+			
+// 		} else{
+// 			console.log('No todo found');
+// 		}
+// 	}) 
+// 	// 	console.log('Finished!');
+// 	// 	console.log(todo);
+// 	// }) 
+// 	.catch(function(e) {
+// 		console.log(e);
+// 	});
+// });
+
